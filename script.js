eb2451c47dbb0242df91d384f7effece92bca28f
Siddhant Pathak <138594273+siddhant2244@users.noreply.github.com>
2024-07-13 22:32:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    function updateClock() {
+        const clockElement = document.getElementById('time');
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+    }
+
+    setInterval(updateClock, 1000);
+    updateClock();
+});
